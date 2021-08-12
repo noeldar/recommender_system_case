66f17a39db60ee35fedfb6888e7006debed08bab
Isiksu <sksueksioglu@gmail.com>
2021-08-12 09:43:50 +0300

    updating preprocessings and test

---
+++ preprocessing/preprocess_tests.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from testbook import testbook
+
+
+@testbook('preprocessing2.ipynb', execute=True)
+def test_func(tb):
+   func = tb.get("filter_negative_price")
+
+   events_df = pd.DataFrame()
+   events_df['price'] = [1,-1,10,-10,3]
+
+   assert len(func(events_df)) == 3
